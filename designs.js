4a7d54395b24e5fa6e1857dab23e011f03c798fd
kwrish <kwrish@users.noreply.github.com>
2018-02-06 22:15:41 +0200

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,54 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+
+  // Select color input
+  // Select size input
+  // When size is submitted by the user, call makeGrid()
+
+  $(document).ready(function() {
+
+  var canvas, table, form, color, gridHeight, gridWidth;
+  canvas=$('#pixelCanvas');
+  table=$('table');
+  form=$('form');
+  color=$('input[type="color"]');
+  gridHeight=$('#inputHeight');
+  gridWidth=$('#inputWidth');
+
+  function makeGrid() {
+  canvas.html('');
+     canvas.css('background-color','white');
+  for(var x=1; x<=gridHeight.val() ;x++){
+    canvas.append('<tr></tr>');}
+    for(var y=1; y<=gridWidth.val() ;y++){
+    $('tr').append('<td></td>');
+  }}
+
+  form.submit(function(e){
+    e.preventDefault();
+    makeGrid();
+  });
+
+  //click to color
+  canvas.on('click','td',function(evt){
+  $(evt.target).css('background-color',color.val());
+  });
+
+  //doubleclick to remove color
+  table.on('dblclick','td',function(evt){
+    $(evt.target).css('background-color','white');
+  });
+
+  //drag and draw
+  canvas.on('mouseover','td', function (event){
+    event.preventDefault();
+  if (event.buttons) {
+  $(this).css("background-color",color.val());
+  }
+  });
+
+
+  });
